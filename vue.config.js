70ef7dc6ae4e3af736ceb7b6b16e46d24b3bb536
Julián Lora <julian.lora.96@gmail.com>
2024-10-26 13:52:16 -0300

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('@vue/cli-service');
+// const { defineConfig } = require('@vue/cli-service');
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
-});
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+// });
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'docs'  // Esto hará que Vue compile en una carpeta 'docs' en lugar de 'dist'
+}
