034002407681e43dd8e6cda02729979e85416cb5
Julián Lora <julian.lora.96@gmail.com>
2024-10-26 14:16:15 -0300

    Refactor Vue global types and update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+// const { defineConfig } = require('@vue/cli-service');
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+// });
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' 
     ? '/portfolio/'  // Debe coincidir exactamente con el nombre de tu repositorio
     : '/',
   assetsDir: '', // Esto ayudará a mantener las rutas más simples
+  outputDir: 'docs' 
 }
